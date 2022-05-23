6fc1189af510e608520f99597dd4c96d1582515f
Awwfrack <106082740+Awwfrack@users.noreply.github.com>
2022-05-23 15:44:48 +0100

    Add files via upload

---
+++ harp-master/udunits2/unitToIdMap.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2013 University Corporation for Atmospheric Research
+ *
+ * This file is part of the UDUNITS-2 package.  See the file COPYRIGHT
+ * in the top-level source-directory of the package for copying and
+ * redistribution conditions.
+ */
+#ifndef UT_UNIT_TO_ID_MAP_H_INCLUDED
+#define UT_UNIT_TO_ID_MAP_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Frees resources associated with a unit-system.
+ *
+ * Arguments:
+ *	system		Pointer to the unit-system to have its associated
+ *			resources freed.
+ */
+EXTERNL void
+utimFreeSystem(
+    ut_system*	system);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
